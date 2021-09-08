fdfd1b92eec41ef74e4ab6951d9dd9f307d657b1
Paxton Proctor <pax.proctor@gmail.com>
2021-09-08 15:16:34 -0500

    inClass assignments 9/8/21

---
+++ InclassHomework/PracticePrograms/Employee.java
@@ -0,0 +1,12 @@
+package InclassHomework.PracticePrograms;
+
+public class Employee {
+
+    // class variables
+    String name;
+    int age;
+    String designation;
+    double salary;
+
+    // 
+}
